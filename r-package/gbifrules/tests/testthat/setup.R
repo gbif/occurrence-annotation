75dad7cad9266ad153cc4c3cf7f06cde626479b8
jhnwllr <jhnwllr@gmail.com>
2026-01-26 16:47:44 +0100

    Fix workflow failures - Remove skip_on_ci() to allow tests to run in CI - Add delete_rule_comment to pkgdown reference index - Add R >= 4.1.0 dependency note for native pipe

--- r-package/gbifrules/tests/testthat/setup.R
+++ r-package/gbifrules/tests/testthat/setup.R
@@ -1,8 +1,7 @@
 library(httptest2)
 
-# Skip all tests on CRAN and CI
+# Skip all tests on CRAN
 skip_on_cran()
-skip_on_ci()
 
 # Set environment variable for all tests
 Sys.setenv(GBIFRULES_URL = "http://localhost:8080/occurrence/experimental/annotation/")
